4a9a79ebd75ff9045a3932418b755309f073bd62
joaquinromeroparra-droid <joaquinromeroparra@gmail.com>
2025-12-16 07:57:54 +0100

    Add submit button for survey in _app.js

---
+++ encuesta-valencia/pages/_app.js
@@ -0,0 +1,9 @@
+<button
+        onClick={handleSubmit}
+        className="w-full bg-gradient-to-r from-orange-500 to-blue-600 text-white py-4 rounded-lg font-bold text-lg hover:from-orange-600 hover:to-blue-700 transition transform hover:scale-105"
+      >
+        Enviar Encuesta
+      </button>
+    </div>
+  </div>
+</div>
